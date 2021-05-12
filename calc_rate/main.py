f1ab0d85c7351aed7914f037562bb7a987af0113
ren <remremphobia@gmail.com>
2021-05-12 21:08:30 +0900

    config取得

--- calc_rate/main.py
+++ calc_rate/main.py
@@ -1,5 +1,6 @@
 # ガチャの確率計算様モジュール
 import matplotlib.pyplot as plt
+import configparser
 
 def calc_rate(proba, try_num):
     """
@@ -36,24 +37,28 @@ def plot_rate(proba, set_num):
     ax.set_xlabel("Number of trials")
     ax.set_ylabel("Probability of occurrence")
     ax.set_title("rate")
-    
+
     plt.show()
 
 
-def main():
-
-    # 引数として数値を受け取る
-    # 当たりの生起確率
-    proba = 0.04
-    # 試行回数
-    try_num = 5
-    #calc_rate(proba, try_num)
-
-    set_num = 100
-    # 可視化実験
+def main(proba, try_num, set_num):
+    # 期待値計算
+    calc_rate(proba, try_num)
+    # 可視化
     plot_rate(proba, set_num)
+
+def startup():
+    # config取得
+    config_ini = configparser.ConfigParser()
+    config_ini.read('config.ini', encoding='utf-8')
+
+    proba = float(config_ini['CALC_STATE']['Proba'])    # 生起確率
+    try_num = int(config_ini['CALC_STATE']['TryNum'])   # 試行回数
+    set_num = int(config_ini['CALC_STATE']['SetNum'])   # 回数
+    main(proba,try_num,set_num)
 
 
 if __name__ == '__main__':
-    main()
+    startup()
+
 
