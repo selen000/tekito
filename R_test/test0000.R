4667490387aa5a00e212bcacbdb3082d1d314ecf
ren <remremphobia@gmail.com>
2021-06-03 23:29:54 +0900

    R言語テスト用

---
+++ R_test/test0000.R
@@ -0,0 +1,16 @@
+class(iris)
+head(iris)
+tail(iris)
+
+# Sepal.Length‚ÌƒqƒXƒgƒOƒ‰ƒ€‚ğ•`‚­
+hist(iris$Sepal.Length)
+
+# •Ï”‚Í‚±‚ñ‚ÈŠ´‚¶
+a = 4
+print(a)
+print("•¶š—ñ‚Íprint‚Å‚«‚é?")
+
+b = 3
+print(a-b)
+print(a*b)
+print(a/b)
